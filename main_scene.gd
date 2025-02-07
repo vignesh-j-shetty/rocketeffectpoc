b5b681da70dabc56f57536a49c5eaf234f663323
vignesh-j-shetty <vigneshshetty838@gmail.com>
2025-02-07 19:32:05 +0530

    commit 2

---
+++ main_scene.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for node in self.get_children():
+		if node.name == "WorldEnvironment":
+			continue
+		#if node.name != "RocketEngine" and node.name == "ColorRect":
+			#node.modulate =  Color(0.7, 0.7, 0.7)
+			
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ rocket_engine.gd
@@ -0,0 +1,12 @@
+extends Sprite2D
+
+var shader_material: ShaderMaterial
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	shader_material = self.material as ShaderMaterial
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var h = lerp(1.45, 1.6, randf())
+	shader_material.set_shader_parameter("flame_height", h)
