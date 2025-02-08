d056518021a46638e72df378c5ebf708d234063e
vignesh-j-shetty <vigneshshetty838@gmail.com>
2025-02-08 22:38:33 +0530

    Improved smoke effect

--- main_scene.gd
+++ main_scene.gd
@@ -3,13 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for node in self.get_children():
-		if node.name == "WorldEnvironment":
-			continue
-		if node.name != "RocketEngine":
-			node.modulate =  Color(0.7, 0.7, 0.7)
-			
-	pass # Replace with function body.
+	#for node in self.get_children():
+		#if node.name == "WorldEnvironment":
+			#continue
+		#if node.name != "RocketEngine":
+			#node.modulate =  Color(0.6, 0.6, 0.6)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- rocket_engine.gd
+++ rocket_engine.gd
@@ -1,26 +1,42 @@
 extends Sprite2D
 
 var shader_material: ShaderMaterial
+var fast_lite_noise: FastNoiseLite
 
 var flame_height: float = 1.0
-const HEIGHT_MIN: float = 0.5
-const HEIGHT_MAX: float = 1.4
-const HEIGHT_STEP: float = 0.05
-
+var smoke_speed: float = 1.8
+var noise_freq = 0.0268
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	shader_material = self.material as ShaderMaterial
+	var texture = NoiseTexture2D.new()
+	fast_lite_noise = FastNoiseLite.new()
+	fast_lite_noise.frequency = noise_freq
+	texture.noise = fast_lite_noise
+	await texture.changed
+	shader_material.set_shader_parameter("noise_tex1", texture)
+	set_process_input(false)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	var flicker = randf() * 0.09 - 0.25
-	var dynamic_height = clamp(flame_height + flicker, HEIGHT_MIN, HEIGHT_MAX)
+	var dynamic_height = flame_height + flicker
+	dynamic_height =  clamp(dynamic_height, 0.5, 1.4)
 	shader_material.set_shader_parameter("flame_height", dynamic_height)
-	_process_input()
+	shader_material.set_shader_parameter("effect_speed", smoke_speed)
+	process_input()
+	fast_lite_noise.frequency = noise_freq
 	
-func _process_input() -> void:
+func process_input() -> void:
 	if Input.is_action_pressed("ui_up"):
-		flame_height = clamp(flame_height + HEIGHT_STEP, HEIGHT_MIN, HEIGHT_MAX)
+		control_flame(1)
 	elif Input.is_action_pressed("ui_down"):
-		flame_height = clamp(flame_height - HEIGHT_STEP, HEIGHT_MIN, HEIGHT_MAX)
+		control_flame(-1)
+		
+func control_flame(dir):
+	flame_height =  clamp(flame_height + (0.05 * dir), 0.5, 1.6)
+	smoke_speed = clamp(smoke_speed + (0.05 * dir), 1.8, 2.2)
+	if flame_height < 0.9:
+		noise_freq = 0.0
+	noise_freq = clamp(noise_freq + (0.005 * dir), 0.02, 0.08)
