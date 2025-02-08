61b585be7268c290a973e129f88cd12794d8d135
vignesh-j-shetty <vigneshshetty838@gmail.com>
2025-02-08 17:35:06 +0530

    better effect implmented

--- rocket_engine.gd
+++ rocket_engine.gd
@@ -1,6 +1,12 @@
 extends Sprite2D
 
 var shader_material: ShaderMaterial
+
+var flame_height: float = 1.0
+const HEIGHT_MIN: float = 0.5
+const HEIGHT_MAX: float = 1.4
+const HEIGHT_STEP: float = 0.05
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	shader_material = self.material as ShaderMaterial
@@ -8,5 +14,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	var h = lerp(1.45, 1.6, randf())
-	shader_material.set_shader_parameter("flame_height", h)
+	var flicker = randf() * 0.09 - 0.25
+	var dynamic_height = clamp(flame_height + flicker, HEIGHT_MIN, HEIGHT_MAX)
+	shader_material.set_shader_parameter("flame_height", dynamic_height)
+	_process_input()
+	
+func _process_input() -> void:
+	if Input.is_action_pressed("ui_up"):
+		flame_height = clamp(flame_height + HEIGHT_STEP, HEIGHT_MIN, HEIGHT_MAX)
+	elif Input.is_action_pressed("ui_down"):
+		flame_height = clamp(flame_height - HEIGHT_STEP, HEIGHT_MIN, HEIGHT_MAX)
